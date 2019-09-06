df3d6d3a486d64393e22df2546c7c9a4fe29fd77
bls51 <54688097+bls51@users.noreply.github.com>
2019-09-06 16:52:32 -0500

    yeet

---
+++ Lab1.java
@@ -0,0 +1,10 @@
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+	System.out.println("bls51:BenjaminLeeSuggs");
+	System.out.println("2 4 8 16 32 64 128 256 512 1024");
+
+	}
+
+}
